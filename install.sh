0b61e935c0f42cf579b36288aa3145f786df7f49
Michaël Journo <gobadiah@gmail.com>
2018-04-27 02:00:04 +0200

    Add kubectl to install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,12 @@
 apt-get install -y vim curl wget dnsutils procps jq python python-pip git
 
 pip install -U awscli
+
+# Kubectl
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubectl
